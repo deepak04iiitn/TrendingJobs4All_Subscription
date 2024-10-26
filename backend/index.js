3b1e6c8aec08df7d055b1cc9681c93d6ba864109
Deepak Kumar Yadav <dky422003@gmail.comm>
2024-10-26 21:27:43 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path'
 
 dotenv.config();
 
@@ -12,9 +13,20 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(cors());
+
+app.use(express.json());
 
 app.listen(4000 , () => {
     console.log('Server is running on port 4000!')
 })
+
+app.use(express.static(path.join(__dirname, '/frontend/dist'))); 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
